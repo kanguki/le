fac43e4eb03de3acb78cf1185d79ee28dd180d05
Mo Nguyen <mo.nguyen@difisoft.com>
2022-04-11 15:35:50 +0700

    update readme, env

--- env.go
+++ env.go
@@ -4,9 +4,6 @@
 package leaderelection
 
 const (
-	//log
-	LOG_PATH = "LOG_PATH" //string. log path. default log to stdout
-
 	//le
 	//nats
 	NATS_QUORUM = "NATS_QUORUM" //comma separated. default: nats://127.0.0.1:4222. reference: https://github.com/nats-io/go-nats/blob/master/example_test.go
